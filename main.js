78e03adeb478ad83d427dcb7622083344f8b8a04
sebastiano minotti <seba.minotti@gmail.com>
2021-09-10 18:01:17 +0200

    completamente riscritto
    ho completamente riscritto il codice html css js ho inserito anche una piccola animazione dell'hamburger icon creato la pagina del menu hamburger

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-$('#ham').click(function () { 
-    $('.sub_menu').addClass('active')
-});
-$('#clos').click(function () { 
-    $('.sub_menu').removeClass('active')
-});
+$('.open').click(function(){
+$('.menu_hamburger').addClass('active')
+})
+
+$('.open').mouseenter(function(){
+    $('.open').css({'transform':'rotate( 180deg )','transition':'3s','box-shadow':'0px 0px 3px 3px white'})
+})
+$('.open').mouseleave(function(){
+    $('.open').css({'transform':'rotate( 0deg)','transition':'3s','box-shadow':'0px 0px 0px 0px white'})
+})
+
+
+$('.close').click(function(){
+    alert('chiuderai il menu')
+    $('.menu_hamburger').removeClass('active')
+    })
